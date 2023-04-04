4c53ba61118f528d073b26fa3b34c1b2c4dfb152
ScottCRed <ScottC@redpandasoftware.co.za>
2023-04-04 15:27:02 +0200

    Add basic API call and log the information

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+let place = 'London';
+
+const newLocation = document.querySelector('.submit-button');
+newLocation.addEventListener('click', changeLocation);
+
+function changeLocation () {
+    event.preventDefault();
+    const searchKey = document.querySelector('form');
+    palce = searchKey.Keyword.value;
+    console.log(place);
+    getImage();
+    searchKey.reset();
+}
+
 async function getWeather() {
     try {
-const response = await fetch(`${keyword}`, {mode:"cors"});
+const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=3ca24ea75ac143c4945115016230404&q=${place}`, {mode:"cors"});
 const weatherData = await response.json();
-console.log(weatherData.data)
+console.log(weatherData.current)
     }
     catch (error) {
         consolee.log('error');
     }
 
 }
+
+getWeather();
