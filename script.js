446d3bbf4e92b35187560cc5d600dfaa66f726b1
ScottCRed <ScottC@redpandasoftware.co.za>
2023-04-04 15:58:10 +0200

    Add changeLocation function and displayInfo dummy function

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ let place = 'London';
 
 const newLocation = document.querySelector('.submit-button');
 newLocation.addEventListener('click', changeLocation);
+const render = document.querySelector('submit-button');
+render.addEventListener('click', displayInfo);
 
 function changeLocation () {
     event.preventDefault();
     const searchKey = document.querySelector('form');
-    palce = searchKey.Keyword.value;
+    place = searchKey.location.value;
     console.log(place);
-    getImage();
+    getWeather();
     searchKey.reset();
 }
 
@@ -16,12 +18,38 @@ async function getWeather() {
     try {
 const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=3ca24ea75ac143c4945115016230404&q=${place}`, {mode:"cors"});
 const weatherData = await response.json();
-console.log(weatherData.current)
+console.log(weatherData)
+return weatherData;
     }
     catch (error) {
         consolee.log('error');
     }
 
+}
+
+function displayInfo () {
+    getWeather();
+    const displayInfo = document.querySelector('.infoDisplay');
+    const place = document.createElement('dive');
+    place.classList.add('.info');
+    place.textContent = 'Location:' + weatherData.Location.name + ' ' + weatherData.Location.country + '.';
+
+    const temp = document.createElement('div');
+    temp.classList.add('info');
+    temp.textContent = 'Current Temperature: ' + weatherData.current.temp_c + ' Celcius.';
+
+    const humid = document.createElement('div');
+    humid.classList.add('info');
+    humid.textContent = 'Current humidity: ' + weatherData.current.humidity + '%';
+
+    const rain = document.createElement('div');
+    rain.classList.add('info');
+    rain.textContent = 'Predicted precipitation: ' + weatherData.current.precip_mm + 'mm';
+
+    displayInfo.appendChild(place);
+    displayInfo.appendChild(temp);
+    displayInfo.appendChild(humid);
+    displayInfo.appendChild(rain);
 }
 
 getWeather();
